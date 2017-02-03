38a3d5b901dc15fb53ea3984afe689803846a05b
Asim Hussain <jawache@gmail.com>
2017-02-03 17:39:25 +0000

    Removed AMD definitions so it plays nice with hybrid Angular 1+2 apps

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -5,15 +5,7 @@
  */
 // wrap in UMD - see https://github.com/umdjs/umd/blob/master/jqueryPluginCommonjs.js
 (function(factory) {
-    if (typeof define === "function" && define.amd) {
-        define([ "jquery" ], function($) {
-            factory($, window, document);
-        });
-    } else if (typeof module === "object" && module.exports) {
-        module.exports = factory(require("jquery"), window, document);
-    } else {
-        factory(jQuery, window, document);
-    }
+    factory(jQuery, window, document);
 })(function($, window, document, undefined) {
     "use strict";
     // these vars persist through all instances of the plugin
